81f6ef633e4428db7e6d2f5af5761b4f6c4d35e1
AlfonsoCavallo <alfonso.cavallo98@gmail.com>
2021-06-14 15:24:59 +0200

    Started implementation of Evento più bello del gioco

--- Data/Scripts/003_Game classes/008_Game_Player_Visuals.rb
+++ Data/Scripts/003_Game classes/008_Game_Player_Visuals.rb
@@ -57,7 +57,7 @@ class Game_Player < Game_Character
       self.move_speed = 4     # Sliding on ice
     elsif !moving? && !@move_route_forcing && $PokemonGlobal
       if $PokemonGlobal.bicycle
-        self.move_speed = 5   # Cycling
+        self.move_speed = 6   # Cycling
       elsif pbCanRun? || $PokemonGlobal.surfing
         self.move_speed = 4   # Running, surfing
       else
